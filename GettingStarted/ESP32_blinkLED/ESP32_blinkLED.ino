c89235038c966bd972972dbeb2fc17293fa5741c
thihaElec <thiha.sales@gmail.com>
2019-02-28 10:17:46 +0630

    Add files via upload

---
+++ GettingStarted/ESP32_blinkLED/ESP32_blinkLED.ino
@@ -0,0 +1,14 @@
+#define LEDpin 2 //on board blueLED
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LEDpin,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LEDpin,HIGH);
+  delay(1000);
+  digitalWrite(LEDpin,LOW);
+  delay(1000);
+}
